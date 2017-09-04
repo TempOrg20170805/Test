1bf0273ebfb1d8aa08d02d7deba49c2b82c0e362
publiu <1154979487@qq.com>
2017-09-04 22:20:21 +0800

    简单修改

--- src/com/sunrun/common/util/weixinpay/ConstantUtil.java
+++ src/com/sunrun/common/util/weixinpay/ConstantUtil.java
@@ -10,9 +10,9 @@ public class ConstantUtil {
 	public static String GATEURL = "https://api.mch.weixin.qq.com/pay/unifiedorder"; //接口链接 URL地址
 	public static String APIKEY = "qyfu0oc8se4k9q3ob5agb4y6lvmtqlsw"; // 商户密钥 微信商户平台-	API安全-API密钥自己填写的32位字符串
 */
-	public static String APPID = "wx5251cf36a7b56931";// 公众账号ID 微信开放平台-创建移动应用时对应的APPID
-	public static String MCH_ID = "1306112001";// 商户号
+	public static String APPID = "wxee81750768f5a9e8";// 公众账号ID 微信开放平台-创建移动应用时对应的APPID
+	public static String MCH_ID = "1488546472";// 商户号
 	public static String GATEURL = "https://api.mch.weixin.qq.com/pay/unifiedorder"; //接口链接 URL地址
-	public static String APIKEY = "2YrnmLxKkA7jSXQieBfkxpHxH5QTzNrn"; // 商户密钥 微信商户平台-	API安全-API密钥自己填写的32位字符串
+	public static String APIKEY = "tP7iWYjKDxQXYQKkHxwVcL7a40E9YfPY"; // 商户密钥 微信商户平台-	API安全-API密钥自己填写的32位字符串
 
 }
